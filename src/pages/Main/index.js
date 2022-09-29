7bd7afa8c3f41064477ccba4d3fc7f1c139311fb
Luis Felipe <luisfelipe.szn@gmail.com>
2022-09-28 23:51:46 -0300

    feat: import styled components from styles.js

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+const Main = () => {
+  return (
+    <S.Container>
+      <S.Header />
+      <S.Footer />
+    </S.Container>
+  );
+};
+
+export default Main;
