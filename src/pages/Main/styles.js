3f50de3e8b791576b96d3c6d03050902c6d74773
Luis Felipe <luisfelipe.szn@gmail.com>
2022-09-28 23:51:26 -0300

    style: create a styled container

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  background: #0d1317;
+`;
+
+export const Footer = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 90px;
+  bottom: 0px;
+  background: #161c21;
+`;
+
+export const Header = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 90px;
+  top: 0px;
+  background: #161c21;
+`;
