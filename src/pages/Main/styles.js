a7cfa4a566b413a0bc3d013d1f9da670defa058c
Luis Felipe <luisfelipe.szn@gmail.com>
2022-10-18 11:54:06 -0300

    style: styling mark button at main

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -21,3 +21,23 @@ export const Header = styled.View`
   top: 0px;
   background: #161c21;
 `;
+
+export const ButtonContainer = styled.View`
+  position: absolute;
+  bottom: 50px;
+  background: #0d1317;
+  border-radius: 50px;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  width: 95px;
+  height: 95px;
+  z-index: 1;
+`;
+
+export const MarkLocationButton = styled.TouchableOpacity`
+  background: #127983;
+  width: 75px;
+  height: 75px;
+  border-radius: 50px;
+`;
