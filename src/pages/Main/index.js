d83ada107d255d03dea3ddbc028a9c8dbc487cbc
Luis Felipe <luisfelipe.szn@gmail.com>
2022-10-18 11:53:49 -0300

    feat: add mark button at main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Text} from 'react-native';
 import * as S from './styles';
 const Main = () => {
   return (
     <S.Container>
       <S.Header />
+      <S.ButtonContainer>
+        <S.MarkLocationButton />
+      </S.ButtonContainer>
       <S.Footer />
     </S.Container>
   );
